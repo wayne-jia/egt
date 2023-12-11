0d9be22f408bbf3eb44dac3a258f02409b5330b7
Wayne Jia <wayne.jia@microchip.com>
2023-12-11 18:11:25 +0800

    Add the serialize/deserialize feature

--- src/detail/erawimage.h
+++ src/detail/erawimage.h
@@ -145,26 +145,74 @@ public:
         return surface;
     }
 
-    static shared_cairo_surface_t load(const unsigned char* buf, size_t len)
+    static shared_cairo_surface_t load(const std::string& filename, std::shared_ptr<egt::Rect>& rect)
     {
-        const auto buf_end = buf + len;
+        std::ifstream i(filename, std::ios_base::binary);
+        if (!i)
+            return nullptr;
         alignas(4) uint32_t magic = 0;
         alignas(4) uint32_t width = 0;
         alignas(4) uint32_t height = 0;
+        alignas(4) int32_t x = 0;
+        alignas(4) int32_t y = 0;
+        alignas(4) uint32_t reserved = 0;
+        i.read(reinterpret_cast<char*>(&magic), sizeof(magic));
 
-        buf = readw(buf, magic, buf_end);
-        if (!buf)
-            return nullptr;
         if (magic != egt_magic())
             return nullptr;
-        buf = readw(buf, width, buf_end);
-        if (!buf)
+        if (!i.read(reinterpret_cast<char*>(&width), sizeof(width)) ||
+            !i.read(reinterpret_cast<char*>(&height), sizeof(height)) ||
+            !i.read(reinterpret_cast<char*>(&x), sizeof(x)) ||
+            !i.read(reinterpret_cast<char*>(&y), sizeof(y)) ||
+            !i.read(reinterpret_cast<char*>(&reserved), sizeof(reserved)) ||
+            !i.read(reinterpret_cast<char*>(&reserved), sizeof(reserved)))
             return nullptr;
-        buf = readw(buf, height, buf_end);
-        if (!buf)
-            return nullptr;
-        buf += (sizeof(uint32_t) * 4);
 
+        rect->x(x);
+        rect->y(y);
+        rect->width(width);
+        rect->height(height);
+
+        auto surface =
+            shared_cairo_surface_t(cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
+                                   width, height),
+                                   cairo_surface_destroy);
+        auto data =
+            reinterpret_cast<uint32_t*>(cairo_image_surface_get_data(surface.get()));
+        auto end = data + (width * height);
+        while (data < end)
+        {
+
+            alignas(4) uint16_t block = 0;
+            if (!i.read(reinterpret_cast<char*>(&block), sizeof(block)))
+                return nullptr;
+            if (block & 0x8000)
+            {
+                block &= 0x7fff;
+                alignas(4) uint32_t value = 0;
+                if (!i.read(reinterpret_cast<char*>(&value), sizeof(value)))
+                    return nullptr;
+                memset32(data, value, block);
+            }
+            else if (block)
+            {
+                if (!i.read(reinterpret_cast<char*>(data), block * sizeof(uint32_t)))
+                    return nullptr;
+            }
+            data += block;
+
+        }
+
+        i.close();
+
+        // must mark surface dirty once we manually fill it in
+        cairo_surface_mark_dirty(surface.get());
+
+        return surface;
+    }
+
+    static shared_cairo_surface_t read_surface_data(const unsigned char* buf, const unsigned char* buf_end, uint32_t width, uint32_t height)
+    {
         auto surface =
             shared_cairo_surface_t(cairo_image_surface_create(CAIRO_FORMAT_ARGB32,
                                    width, height),
@@ -203,6 +251,65 @@ public:
         cairo_surface_mark_dirty(surface.get());
 
         return surface;
+    }
+
+    static shared_cairo_surface_t load(const unsigned char* buf, size_t len)
+    {
+        const auto buf_end = buf + len;
+        alignas(4) uint32_t magic = 0;
+        alignas(4) uint32_t width = 0;
+        alignas(4) uint32_t height = 0;
+
+        buf = readw(buf, magic, buf_end);
+        if (!buf)
+            return nullptr;
+        if (magic != egt_magic())
+            return nullptr;
+        buf = readw(buf, width, buf_end);
+        if (!buf)
+            return nullptr;
+        buf = readw(buf, height, buf_end);
+        if (!buf)
+            return nullptr;
+        buf += (sizeof(uint32_t) * 4);
+
+        return read_surface_data(buf, buf_end, width, height);
+    }
+
+    static shared_cairo_surface_t load(const unsigned char* buf, size_t len, std::shared_ptr<egt::Rect>& rect)
+    {
+        const auto buf_end = buf + len;
+        alignas(4) uint32_t magic = 0;
+        alignas(4) uint32_t width = 0;
+        alignas(4) uint32_t height = 0;
+        alignas(4) int32_t x = 0;
+        alignas(4) int32_t y = 0;
+
+        buf = readw(buf, magic, buf_end);
+        if (!buf)
+            return nullptr;
+        if (magic != egt_magic())
+            return nullptr;
+        buf = readw(buf, width, buf_end);
+        if (!buf)
+            return nullptr;
+        buf = readw(buf, height, buf_end);
+        if (!buf)
+            return nullptr;
+        buf = readw(buf, x, buf_end);
+        if (!buf)
+            return nullptr;
+        buf = readw(buf, y, buf_end);
+        if (!buf)
+            return nullptr;
+        buf += (sizeof(uint32_t) * 2);
+
+        rect->x(x);
+        rect->y(y);
+        rect->width(width);
+        rect->height(height);
+
+        return read_surface_data(buf, buf_end, width, height);
     }
 
     static uint16_t next_diff_block(uint32_t* data, const uint32_t* end)
@@ -275,6 +382,109 @@ public:
             }
         }
         o.close();
+    }
+
+    static void save(const std::string& path, unsigned char* data, int32_t x, int32_t y, uint32_t width, uint32_t height)
+    {
+        std::ofstream o(path, std::ios_base::binary);
+        const auto magic = egt_magic();
+        o.write(reinterpret_cast<const char*>(&magic), sizeof(magic));
+        o.write(reinterpret_cast<const char*>(&width), sizeof(width));
+        o.write(reinterpret_cast<const char*>(&height), sizeof(height));
+
+        //add x,y coordinate
+        o.write(reinterpret_cast<const char*>(&x), sizeof(x));
+        o.write(reinterpret_cast<const char*>(&y), sizeof(y));
+
+        uint32_t reserved = 0;
+        o.write(reinterpret_cast<const char*>(&reserved), sizeof(reserved));
+        o.write(reinterpret_cast<const char*>(&reserved), sizeof(reserved));
+
+        const auto start = reinterpret_cast<uint32_t*>(data);
+        auto offset = reinterpret_cast<uint32_t*>(data);
+        const auto end = reinterpret_cast<uint32_t*>(start + (width * height));
+
+        while (offset < end)
+        {
+            uint32_t value = 0;
+            auto same = next_same_block(offset, end, value);
+            if (same)
+            {
+                offset += same;
+                same |= 0x8000;
+                o.write(reinterpret_cast<const char*>(&same), sizeof(same));
+                o.write(reinterpret_cast<const char*>(&value), sizeof(value));
+            }
+            else
+            {
+                auto diff = next_diff_block(offset, end);
+                if (diff)
+                {
+                    o.write(reinterpret_cast<const char*>(&diff), sizeof(diff));
+                    o.write(reinterpret_cast<const char*>(offset), diff * sizeof(uint32_t));
+                    offset += diff;
+                }
+            }
+        }
+        o.close();
+    }
+
+    static void save(const std::string& path, unsigned char* data, int32_t x, int32_t y, uint32_t width, uint32_t height, uint32_t* len)
+    {
+        uint32_t length = 0;
+        std::ofstream o(path, std::ios_base::app | std::ios_base::binary);
+        const auto magic = egt_magic();
+        o.write(reinterpret_cast<const char*>(&magic), sizeof(magic));
+        length += sizeof(magic);
+        o.write(reinterpret_cast<const char*>(&width), sizeof(width));
+        length += sizeof(width);
+        o.write(reinterpret_cast<const char*>(&height), sizeof(height));
+        length += sizeof(height);
+
+        //add x,y coordinate
+        o.write(reinterpret_cast<const char*>(&x), sizeof(x));
+        length += sizeof(x);
+        o.write(reinterpret_cast<const char*>(&y), sizeof(y));
+        length += sizeof(y);
+
+        uint32_t reserved = 0;
+        o.write(reinterpret_cast<const char*>(&reserved), sizeof(reserved));
+        o.write(reinterpret_cast<const char*>(&reserved), sizeof(reserved));
+        length += (2 * sizeof(reserved));
+
+        const auto start = reinterpret_cast<uint32_t*>(data);
+        auto offset = reinterpret_cast<uint32_t*>(data);
+        const auto end = reinterpret_cast<uint32_t*>(start + (width * height));
+
+        while (offset < end)
+        {
+            uint32_t value = 0;
+            auto same = next_same_block(offset, end, value);
+            if (same)
+            {
+                offset += same;
+                same |= 0x8000;
+                o.write(reinterpret_cast<const char*>(&same), sizeof(same));
+                length += sizeof(same);
+                o.write(reinterpret_cast<const char*>(&value), sizeof(value));
+                length += sizeof(value);
+            }
+            else
+            {
+                auto diff = next_diff_block(offset, end);
+                if (diff)
+                {
+                    o.write(reinterpret_cast<const char*>(&diff), sizeof(diff));
+                    length += sizeof(diff);
+                    o.write(reinterpret_cast<const char*>(offset), diff * sizeof(uint32_t));
+                    length += diff * sizeof(uint32_t);
+                    offset += diff;
+                }
+            }
+        }
+        o.close();
+
+        *len = length;
     }
 
 };
---
+++ tools/gfxconvert/README.md
@@ -0,0 +1,48 @@
+# gfxconvert tool 
+
+This tool is used to convert SVG or PNG/JPG/BMP file into eraw.bin which can be
+loaded by EGT code directly to make graphic render fast. Usually this tool is 
+running on your Linux host PC, because it has no meaning if you convert graphics
+on embedded target.
+
+## Usage Example
+
+1. Before using this tool, please make sure that you've followed the egt build
+instruction to build egt library ready.
+```
+	git clone --recursive https://github.com/linux4sam/egt.git
+	cd egt
+	./autogen.sh
+	./configure
+	make
+```
+2. Make the gfxconvert tool.
+```
+	cd egt/tool/gfxconvert
+	make
+```
+3. Start the gfxconvert, of course, you can run ./gfxconvert -h to get help.
+
+```
+	./gfxconvert -s
+	./gfxconvert -i img picture1.png
+	./gfxconvert -i img picture2.jpg
+		......
+	./gfxconvert -i img pictureN.bmp
+	./gfxconvert file.svg
+	./gfxconvert -e
+```
+4. When conversion finished, copy ./eraw.h to source code to include in your
+application cpp code, and copy ./eraw.bin to the target. At last your application
+will read eraw.bin on target to get the graphics data.
+
+5. The generated eraw.h has an array inidicating the graphics id with offset and
+length, and it has an array index in the comment. So when you are coding, you could
+use the array index directly without iterating the array to find the graphics you
+want. There is an example in egt/examples/motorcycledash.cpp, please refer to that
+to learn how to use the eraw.h and eraw.bin. 
+
+## License
+
+EGT is released under the terms of the `Apache 2` license. See the [COPYING](../COPYING)
+file for more information.
