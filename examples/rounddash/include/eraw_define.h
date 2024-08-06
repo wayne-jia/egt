664a13633d6fff18249ca3eb840bc74effaeab30
Wayne Jia <wayne.jia@microchip.com>
2024-08-06 12:24:15 +0800

    Update the *eraw.h and *eraw.bin files which generated by new gfx-convert tool

--- examples/rounddash/include/eraw_define.h
+++ examples/rounddash/include/eraw_define.h
@@ -3,15 +3,15 @@
  *
  * SPDX-License-Identifier: Apache-2.0
  */
-#ifndef __ERAW_DEFINE_H__	
-#define __ERAW_DEFINE_H__	
-	
-#include <string>	
-	
-typedef struct {	
-    std::string name;	
-    int offset;	
-    int len;	
-} eraw_st;	
+#ifndef __ERAW_DEFINE_H__
+#define __ERAW_DEFINE_H__
 
-#endif	
+#include <string>
+
+typedef struct {
+    std::string name;
+    int offset;
+    int len;
+} eraw_st;
+
+#endif
--- examples/rounddash/include/stage1_eraw.h
+++ examples/rounddash/include/stage1_eraw.h
@@ -1,14 +1,25 @@
-#include "eraw_define.h"	
+/*
+ * Copyright (C) 2018 Microchip Technology Inc.  All rights reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#ifndef __Speedo_H__
+#define __Speedo_H__
 
-eraw_st Speedo_table[] = {	
-    {"Speedo", 0, 884338},    //0	
-    {"logobg", 884338, 11958},    //1	
-    {"MicrochipLogo", 896296, 17728},    //2	
-    {"infobg", 914024, 34},    //3	
-    {"Speedo-blue-blur", 914058, 982386},    //4	
-    {"StA", 1896444, 1310},    //5	
-    {"RtA", 1897754, 836},    //6	
-    {"LtA", 1898590, 828},    //7	
-    {"destinationIcon", 1899418, 2766},    //8	
-    {"N000_157_422_188x300", 1902184, 9458},    //9	
+#include "eraw_define.h"
+
+eraw_st Speedo_table[] = {
+    {"Speedo", 0, 884338},    //0
+    {"logobg", 884338, 11958},    //1
+    {"MicrochipLogo", 896296, 17728},    //2
+    {"infobg", 914024, 34},    //3
+    {"Speedo-blue-blur", 914058, 982386},    //4
+    {"StA", 1896444, 1310},    //5
+    {"RtA", 1897754, 836},    //6
+    {"LtA", 1898590, 828},    //7
+    {"destinationIcon", 1899418, 2766},    //8
+    {"N000_157_422_188x300", 1902184, 9458},    //9
 };
+
+#endif
+
